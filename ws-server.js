71b583a24ca3a7026afbc34d3d10b450ecb582ee
hwilliamssl <hwilliams@scottlogic.com>
2020-06-09 16:21:19 +0100

    updated name response regex

--- ws-server.js
+++ ws-server.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-RegExp.prototype.append = function(re) {
+RegExp.prototype.append = function (re) {
   return new RegExp(this.source + re.source, this.flags);
 }
 
@@ -17,8 +17,17 @@ function startWsServer(wsPort) {
     console.log("This conversation is over!")
   })
 
+  const botQuestions = {
+    NO_QUESTION: undefined,
+    HOW_ARE_YOU: 'How are you today?',
+    WHO_ARE_YOU: `What's your name?`,
+    WHERE_ARE_YOU: `Where do you live?`
+  }
+
   wss.on('connection', (ws) => {
-    ws.send(`Hi, \n Thanks for joining me for a chat. How are you today?`)
+    let botQuestion = botQuestions.WHO_ARE_YOU
+    let clientName
+    ws.send(`Hi, \n Thanks for joining me for a chat. ${botQuestion}`)
 
     ws.on('message', (message) => {
       console.log('received: %s', message)
@@ -29,33 +38,54 @@ function startWsServer(wsPort) {
       let response = ''
       let clientFeelsGood = false
 
-      // const wellnessRegex = /(i('?| a)m)?.*((fine|good|(very well)|well|ok(ay)?|alright|great|cool|sweet|not( too)? bad|happy)|(sad|bad|sick|ill|bored))( thank(you|s))?.*/i
-      let wellnessRegex = /(i('?| a)m)?.*/i
-      .append(/(fine|good|(very well)|well|ok(ay)?|alright|great|cool|sweet|not( too)? bad|happy)/)
-      .append(/|(sad|bad|sick|ill|bored|tired)/)
-      .append(/( thank(you|s))?.*/)
-      console.log (wellnessRegex)
-      const clientWellness = trimmedMessage.match(wellnessRegex)
-      if(clientWellness){
-        console.log(clientWellness)
-        clientWellness.forEach((match, index) => console.log(`Group ${index} : ${match}`))
-        clientFeelsGood = typeof clientWellness[3] !== 'undefined' ? true : false
-        response += clientFeelsGood ?  `I'm glad to hear you're ${clientWellness[3]}. ` : `I'm so sorry to hear you're ${clientWellness[7]}. `
-        iGetIt = true
-      }
+      const clientNameRespRegex = /(((my|the) name( i|'?)s )|(([a-zA-Z]+ )?calls? me )|(i(( a|'?)m ))((called? )|(known as ))?|(i go by the name ))?(([\w-']+ ?)+)/igm
       
 
-      if(trimmedMessage.includes('how are you?', trimmedMessage.length-16)){
-        ws.send(response += `I'm fine, Thanks for asking. `)
-        return
+      if(botQuestion === botQuestions.WHO_ARE_YOU){
+
+        const clientNameGroups = trimmedMessage.match(clientNameRespRegex)
+        if(clientNameGroups){
+          clientNameGroups.forEach((match, index) => console.log(`Group ${index} : ${match}`))
+          clientName = clientNameGroups[14]
+          response += `Hi ${clientName}. Great name! `
+          iGetIt = true
+        }
       }
 
-      if(trimmedMessage.includes('?', trimmedMessage)){
+      const clientWellnessRegex = /(i('?| a)m)?.*/igm
+        .append(/\s(fine|good|(very well)|well|ok(ay)?|alright|great|cool|sweet|not( too)? bad|happy)/)
+        .append(/|\s(sad|bad|sick|ill|bored|tired)/)
+        .append(/\s(thank(you|s))?.*/)
+
+      const clientWellness = trimmedMessage.match(clientWellnessRegex)
+
+      if (clientWellness) {
+        console.log(clientWellness)
+        clientFeelsGood = typeof clientWellness[3] !== 'undefined' ? true : false
+        response += clientFeelsGood ? `I'm glad to hear you're ${clientWellness[3]}. ` : `I'm so sorry to hear you're ${clientWellness[7]}. `
+        iGetIt = true
+      }
+
+
+      // client asks about bot wellbeing
+      let askChatbotWellnessRegex = /(((and )?(how a?re)? )?((yo)?u)|(yourself))\??/i
+      const chatBotWellbeingAsked = trimmedMessage.match(askChatbotWellnessRegex)
+      if (chatBotWellbeingAsked) {
+        response += `I'm fine, Thanks for asking. `
+        iGetIt = true
+      }
+
+      if (trimmedMessage.includes('?')) {
+        // client asks bot's name
+        //(^\w+)$|(my\sname(\si|'?)s)|(i('?|\sa)m)\s(\w+)(\s\w+)?\.?(\swhat's yours(\?)?)?
+        // client asks about weather
+        // client asks for the time / date
+        // else
         ws.send(`Hmmmm. I don't know, but you could try Google!`)
         return
       }
 
-      response += iGetIt? '' : `What you just said was so enlightening. I'm learning so much about you. `
+      response += iGetIt ? '' : `What you just said was so enlightening. I'm learning so much about you. `
 
       ws.send(response)
 
